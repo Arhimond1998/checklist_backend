b1ba994f28b848606d36406255db6f526ad3c85a
dokhuzhev_ei <dokhuzhev_ei@magnit.ru>
2025-08-16 22:26:29 +0300

    add cors

--- src/main.py
+++ src/main.py
@@ -5,17 +5,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import web_router, auth_router
 from src.migration import init_migration
-app = FastAPI(
-    title="Check list",
-    version="0.1.0",
-    on_startup=[init_migration]
-)
+
+app = FastAPI(title="Check list", version="0.1.0", on_startup=[init_migration])
 
 origins = [
     "http://localhost:3000",
     "http://frontend",
     "http://127.0.0.1:3000",
     "http://127.0.0.1",
+    "http://5.129.200.132",
+    "http://5.129.200.132:3000",
 ]
 
 app.add_middleware(
